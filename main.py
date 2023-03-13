ce8fef48a3a33f63fc16cb01d04d5ca3abb04a0d
Aziza <azizamukashkyzy@gmail.com>
2023-03-13 21:14:14 +0600

    minor correction

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def add_length(str_: str) -> List[str]:
 
 def sum_two_smallest_numbers(numbers: list) -> int:
     """https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python"""
+    # numbers.sort()
+    # return sum(numbers[:2])
 
-    numbers.sort()
-    return sum(numbers[:2])
+    first_min = min(numbers)
+    numbers.remove(first_min)
+    return first_min + min(numbers)
 
 
 def array_diff(a: List[int], b: List[int]) -> List[int]:
@@ -24,6 +27,24 @@ def split_the_bill(group: dict) -> dict:
     """https://www.codewars.com/kata/5641275f07335295f10000d0/train/python"""
     total = sum(group.values())
     average = total / len(group)
+    bill = {
+    }
     for key in group.keys():
-        group[key] = round(group[key] - average, 2)
-    return group
+        bill[key] = round(group[key] - average, 2)
+    return bill
+
+
+def get_larger_numbers(a, b):
+    "https://www.codewars.com/kata/563b1f55a5f2079dc100008a / train / python"
+    return [max(i) for i in zip(a, b)]
+
+
+def solution(s):
+    "https: // www.codewars.com / kata / 515de9ae9dcfc28eb6000001 / solutions / python"
+
+    if len(s) % 2:
+        s = s + "_"
+    return [s[i:i + 2] for i in range(0, len(s), 2)]
+
+
+
